06be735aee38981c418063c1271870ade76f55b0
manonthemat <empower@manonthemat.com>
2014-10-07 22:07:41 -0700

    sdMarkMostUsedWord directive added

--- directives/stringdirectives.js
+++ directives/stringdirectives.js
@@ -26,4 +26,38 @@ angular.module('stringdirectives', [])
     }
   };
 })
+.directive('sdMarkMostUsedWord', function() {
+  function makeHash(str) {
+    var o = {};
+    for(var i=0; i<str.length; i++) {
+      o[str[i]] = o[str[i]]+1 || 1;
+    }
+    return o;
+  }
+  return {
+    restrict: 'EA',
+    link: function(scope, e, attr) {
+      var origtext = e.text().split(' '),
+          theWord = '';
+      var result = makeHash(origtext);
+      // this is going to look stupid, but I'll refactor later
+      // TODO: refactor this, maybe also add some pink
+      var keys = Object.keys(result);
+      for(var i=0, n=0, amountOfKeys=keys.length; i<amountOfKeys; i++) {
+        if(result[keys[i]] > n) {
+          n = result[keys[i]];
+          theWord = keys[i];
+        }
+      }
+      // maybe on mouseover over the word you gotta play a specific media file, especially when the word is the bird
+      e.html(origtext.map(function(word) {
+        if (word === theWord) {
+          return "<span style='background-color:pink;color:white'>"+word+"</span>";
+        } else {
+          return word;
+        }
+      }).join(' '));
+    },
+  };
+})
 ;
